fde09012de52db28a72c20ae7a0dcd3bc4cca7ca
Taha Hameed <tahahameed1@gmail.com>
2025-05-20 16:53:11 +0500

    year dynamic and css fix

--- src/app/ui/components/footer.tsx
+++ src/app/ui/components/footer.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 export default function Footer() {
+    const currentYear = new Date().getFullYear();
+
     return (
         <div id="footer">
             <div className ="container">
                 <div className ="footer">
-                    <p> &copy; 2024 <Link href="#">Taha Hameed</Link> </p>
+                    <p> &copy; {currentYear} <Link href="#">Taha Hameed</Link> </p>
                 </div>
             </div>
         </div>
